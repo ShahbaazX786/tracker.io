473dc0462922fa6b0e2a85a1718c3c415ad45199
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-01-27 19:40:02 +0530

    1.4.5

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -6,7 +6,7 @@ import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const Navbar = () => {
     const currentActivePath = usePathname();
@@ -17,25 +17,32 @@ const Navbar = () => {
         { href: '/issues/list', label: 'Issues' },
     ]
     return (
-        <nav className='flex px-5 h-14 items-center border-b justify-between shadow-sm'>
-            <Link className='flex items-center' href='/'>Tracker.io <AiFillBug className='ml-2' /></Link>
-            <ul className='flex flex-row items-center'>
-                {menu.map((m, index) =>
-                    <li key={index}>
-                        <Link
-                            href={m.href}
-                            className={classnames({
-                                'text-slate-500 font-normal p-4': currentActivePath !== m.href,
-                                'text-slate-900 font-normal p-4 bg-zinc-200': currentActivePath === m.href,
-                                'hover:text-slate-800 transition-colors': true
-                            }
-                            )}>{m.label}</Link>
-                    </li>)}
-            <Box>
-                {status === 'authenticated' && <Link href={'/api/auth/signout'}>Log Out</Link>}
-                {status === 'unauthenticated' && <Link href={'/api/auth/signin'}>Log In</Link>}
-            </Box>
-            </ul>
+        <nav className='px-5 border-b shadow-sm py-4'>
+            <Container>
+                <Flex justify={'between'}>
+                    <Flex align={'center'} gap={'4'}>
+                        <Link className='flex items-center' href='/'>Tracker.io <AiFillBug className='mr-3' /></Link>
+                        <ul className='flex flex-row items-center'>
+                            {menu.map((m, index) =>
+                                <li key={index}>
+                                    <Link
+                                        href={m.href}
+                                        className={classnames({
+                                            'text-slate-500 font-normal p-4': currentActivePath !== m.href,
+                                            'text-slate-900 font-normal p-4 bg-zinc-200': currentActivePath === m.href,
+                                            'hover:text-slate-800 transition-colors': true
+                                        }
+                                        )}>{m.label}</Link>
+                                </li>)}
+
+                        </ul>
+                    </Flex>
+                    <Box>
+                        {status === 'authenticated' && <Link href={'/api/auth/signout'}>Log Out</Link>}
+                        {status === 'unauthenticated' && <Link href={'/api/auth/signin'}>Log In</Link>}
+                    </Box>
+                </Flex>
+            </Container>
         </nav>
     )
 }
--- documentation/changelog.md
+++ documentation/changelog.md
@@ -1,6 +1,8 @@
 ### Tracker.io - A Issue Tracker app made with Nextjs.
 
 
+> 1.4.5 - Setting Up NextAuth 5: Styling Navbar.
+
 > 1.4.4 - Setting Up NextAuth 4: Adding login/logout.
 
 > 1.4.3 - Setting Up NextAuth 3: Prisma Adapter configuration.
