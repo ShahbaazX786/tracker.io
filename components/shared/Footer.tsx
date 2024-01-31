542a2644f668d9a354e76dbd52fd9d60c4a410ee
Avi <avi.leeker@gmail.com>
2024-01-31 13:27:21 +0200

    removed bg-white to look correct with dark mode

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <>
             <Container className="pt-8 pb-4">
-                <footer className="bg-white rounded-lg shadow-md ">
+                <footer className=" rounded-lg shadow-md ">
                     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                         <span className="text-sm text-gray-700 sm:text-center">© 2024  <a href="https://tracker.io.com/" className="hover:underline">Tracker.io™</a>  |  All Rights Reserved.
                         </span>
